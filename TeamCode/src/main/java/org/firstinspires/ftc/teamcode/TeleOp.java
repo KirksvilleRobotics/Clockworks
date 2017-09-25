df08177917cf655b944c4663e0172f039e715717
yearbook <caleb.andrew.flaim@gmail.com>
2017-09-25 16:12:29 -0500

    Basic Movement - Gamepad1 Joysticks and A.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,3 +1,5 @@
+//created by The Clockworks
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -6,19 +8,15 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
-/**
- * Created by karl ramberg on 9/20/17.
- */
-
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 public class TeleOp extends OpMode {
 
     public DcMotor leftDrive, rightDrive;
     public Servo jewelPitch, jewelYaw, glyphPusher;
     
-    public float threshold;
-    public boolean aReleased = true;
-    public boolean aPressed = false;
+    public float threshold = 10;
+    public boolean aAvailable = true;
+
     @Override
     public void init() {
 
@@ -36,39 +34,53 @@ public class TeleOp extends OpMode {
     @Override
     public void loop() {
 
-        /* GAMEPAD1 - Driving */
+        /* GAMEPAD1 - DRIVING */
 
         //Left Stick - power controlled by stick pos
+        //Threshold is because the joysticks are never perfect
         if(gamepad1.left_stick_y > threshold || gamepad1.left_stick_y < -threshold){
             leftDrive.setPower(gamepad1.left_stick_y);
         }
 
         //Right Stick - power controlled by stick pos
+        //Threshold is because the joysticks are never perfect
         if(gamepad1.right_stick_y > threshold || gamepad1.right_stick_y < -threshold){
-            rightDrive.setPower(gamepad1.right_stick_y);
+            rightDrive.setPower(gamepad1.right_stick_y); //TODO convert
         }
 
         // A Button - gradual acceleration
         // only accelerates again after A has been pressed OR released.
-        if(gamepad1.a && aReleased){
+        if(gamepad1.a && aAvailable){ ///0.0 - 1.0
             for(int i = 1; i <= 10; i++){
                 leftDrive.setPower((float)(i*0.1));
                 rightDrive.setPower((float)(i*0.1));
+                try {
+                    wait(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            aReleased = false;
-            aPressed = true;
+            aAvailable = false;
         }
 
-        if(!gamepad1.a && aPressed){
-            for(int i = 10; i >= 1; i--){
+        if(!gamepad1.a && !(aAvailable)){
+            for(int i = 10; i >= 1; i-=2){
                 leftDrive.setPower((float)(i*0.1));
                 rightDrive.setPower((float)(i*0.1));
+                try {
+                    wait(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            aReleased = true;
-            aPressed = false;
+            aAvailable = true;
         }
 
-        /* GAMEPAD2 - Accesories */
+        /* GAMEPAD2 - ACCESSORIES */
 
+        //TopHat - Glyph Pusher
+        if(gamepad2.dpad_up){
+
+        }
     }
 }
